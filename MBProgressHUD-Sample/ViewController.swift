16245d03a6ce8b81e6e6583a092a0506f57010bb
rea-sna <reoshigeno0708@gmail.com>
2021-05-30 11:05:06 +0900

    first commit

--- MBProgressHUD-Sample/ViewController.swift
+++ MBProgressHUD-Sample/ViewController.swift
@@ -6,12 +6,34 @@
 //
 
 import UIKit
+import MBProgressHUD
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func startedButton(_ sender: UIButton) {
+        // HUDの表示
+        MBProgressHUD.showAdded(to: view, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            // HUDの非表示
+            MBProgressHUD.hide(for: self.view, animated: true)
+        }
+    }
+    
+    @IBAction func startedTextButton(_ sender: UIButton) {
+        // HUDの表示
+        let hud = MBProgressHUD.showAdded(to: view, animated: true)
+        // 表示テキストを追加
+        hud.label.text = "Loading..."
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            // HUDの非表示
+            MBProgressHUD.hide(for: self.view, animated: true)
+        }
     }
 
 
